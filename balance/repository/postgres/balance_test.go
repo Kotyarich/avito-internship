866315e27bc79d9506a186d6fffe9323780a013e
Kotyarich <ndkotov@gmail.com>
2021-11-17 20:45:49 +0300

    Integrated database tests done

---
+++ balance/repository/postgres/balance_test.go
@@ -0,0 +1,140 @@
+package postgres
+
+import (
+	"avito-intership/balance"
+	"avito-intership/utils"
+	"database/sql"
+	"github.com/ory/dockertest"
+	"github.com/stretchr/testify/suite"
+	"log"
+	"testing"
+	"time"
+)
+
+const (
+	smallAmount float32 = 100
+	halfAmount float32 = 50
+	bigAmount float32 = 1000
+)
+
+type balanceRepositorySuite struct {
+	suite.Suite
+
+	db *sql.DB
+	pool *dockertest.Pool
+	resource *dockertest.Resource
+
+	repository balance.Repository
+	curId int64
+}
+
+
+func (suite *balanceRepositorySuite) SetupSuite() {
+	db, pool, resource := utils.DockerDBUp()
+	err := utils.InitTable(db, "../../../scripts/init.sql")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	log.Println(err)
+	time.Sleep(100500000)
+
+	repository := NewBalanceRepository(db)
+
+	suite.repository = repository
+	suite.db = db
+	suite.pool = pool
+	suite.resource = resource
+	suite.curId = 0
+}
+
+func (suite *balanceRepositorySuite) TestGetBalance_Zero() {
+	var expectedAmount float32 = 0
+	suite.curId += 1
+	id := suite.curId
+
+	amount, err := suite.repository.GetBalance(id)
+
+	suite.NoError(err, "getting balance should not produce error")
+	suite.Equal(expectedAmount, amount)
+}
+
+func (suite *balanceRepositorySuite) TestChangeBalance_NewBalance() {
+	amount := smallAmount
+	suite.curId += 1
+	id := suite.curId
+
+	err := suite.repository.ChangeBalance(id, amount)
+
+	suite.NoError(err, "changing balance should not produce error")
+}
+
+func (suite *balanceRepositorySuite) TestChangeBalance_TooLow() {
+	amount  := -bigAmount
+	suite.curId += 1
+	id := suite.curId
+
+	err := suite.repository.ChangeBalance(id, smallAmount)
+	suite.NoError(err, "positive changing balance should not produce error")
+
+	err = suite.repository.ChangeBalance(id, amount)
+	suite.EqualError(balance.ErrTooLowBalance, err.Error())
+}
+
+func (suite *balanceRepositorySuite) TestGetBalance_NonZero() {
+	expectedAmount := smallAmount
+	suite.curId += 1
+	id := suite.curId
+
+	err := suite.repository.ChangeBalance(id, smallAmount)
+	suite.NoError(err, "positive changing balance should not produce error")
+
+	amount, err := suite.repository.GetBalance(id)
+
+	suite.NoError(err, "getting balance should not produce error")
+	suite.Equal(expectedAmount, amount)
+}
+
+func (suite *balanceRepositorySuite) TestChangeBalance_Withdraw() {
+	amount := halfAmount
+	suite.curId += 1
+	srcId := suite.curId
+	suite.curId += 1
+	dstId := suite.curId
+
+	err := suite.repository.ChangeBalance(srcId, smallAmount)
+	suite.NoError(err, "positive changing balance should not produce error")
+
+	err = suite.repository.TransferMoney(srcId, dstId, amount)
+	suite.NoError(err, "changing balance should not produce error")
+
+	newAmount, err := suite.repository.GetBalance(srcId)
+
+	suite.NoError(err, "getting balance should not produce error")
+	suite.Equal(amount, newAmount)
+}
+
+func (suite *balanceRepositorySuite) TestTransferMoney_TooLowBalance() {
+	amount := halfAmount
+	suite.curId += 1
+	srcId := suite.curId
+	suite.curId += 1
+	dstId := suite.curId
+
+	err := suite.repository.TransferMoney(srcId, dstId, amount)
+	suite.Equal(balance.ErrTooLowBalance, err)
+}
+
+
+func (suite *balanceRepositorySuite) TearDownSuite() {
+	err := utils.DropTable(suite.db, []string{"balances"})
+	if err != nil {
+		log.Println(err)
+	}
+	if err := suite.pool.Purge(suite.resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+}
+
+func TestBalanceUseCase(t *testing.T) {
+	suite.Run(t, new(balanceRepositorySuite))
+}
---
+++ utils/table_util.go
@@ -0,0 +1,82 @@
+package utils
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
+	"io/ioutil"
+	"log"
+	"time"
+)
+
+func InitTable(db *sql.DB, initFile string) error {
+	c, ioErr := ioutil.ReadFile(initFile)
+	if ioErr != nil {
+		log.Fatal(ioErr)
+	}
+
+	sqlStr := string(c)
+	_, err := db.Exec(sqlStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func DropTable(db *sql.DB, tables []string) error {
+	query := "DROP TABLE IF EXISTS $1 CASCADE"
+
+	var err error = nil
+	for _, table := range tables {
+		_, err = db.Exec(query, table)
+	}
+
+	return err
+}
+
+func DockerDBUp() (*sql.DB, *dockertest.Pool, *dockertest.Resource) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "11",
+		Env: []string{
+			"POSTGRES_PASSWORD=secret",
+			"POSTGRES_USER=user_name",
+			"POSTGRES_DB=dbname",
+			"listen_addresses = '*'",
+		},
+	}, func(config *docker.HostConfig) {
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
+	})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	hostAndPort := resource.GetHostPort("5432/tcp")
+	databaseUrl := fmt.Sprintf("postgres://user_name:secret@%s/dbname?sslmode=disable", hostAndPort)
+
+	log.Println("Connecting to database on url: ", databaseUrl)
+
+	_ = resource.Expire(220)
+
+	var db *sql.DB
+	pool.MaxWait = 120 * time.Second
+	if err = pool.Retry(func() error {
+		db, err = sql.Open("postgres", databaseUrl)
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	return db, pool, resource
+}
